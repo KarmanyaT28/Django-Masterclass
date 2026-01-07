0aef8ba325b4bd8d711454a486c9fb883e0eac2a
karmanya <karmanya100@gmail.com>
2026-01-06 20:45:35 -0500

    Section-9

--- Section-2-22/pro/mysite/myapp/models.py
+++ Section-2-22/pro/mysite/myapp/models.py
@@ -11,5 +11,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default='https://cdn-icons-png.flaticon.com/512/1147/1147805.png')
 
 
