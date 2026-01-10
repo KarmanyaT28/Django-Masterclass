64cf0e6709fa1df4658d483b5513d366ccd2a953
karmanya <karmanya100@gmail.com>
2026-01-10 00:36:53 -0500

    Users app with authentication part

--- Section-2-22/pro/mysite/myapp/urls.py
+++ Section-2-22/pro/mysite/myapp/urls.py
@@ -6,6 +6,9 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('item/',views.item,name='item'),
+    # path('item/',views.item,name='item'),
     path('<int:id>/',views.detail, name='detail'),
+    path('add/',views.create_item, name='create-item'), 
+    path('update/<int:id>/',views.update_item, name='update-item'),
+    path('delete/<int:id>/',views.delete_item, name='delete-item'),
 ]
---
+++ Section-2-22/pro/mysite/users/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+
+# from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
+
+from .forms import RegisterForm
+
+
+
+def register(request):
+
+    if request.method=="POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}, your account has been successfully created')
+
+            return redirect('login')
+    
+    form = RegisterForm()
+
+    context = {
+        'form':form
+    }
+    return render(request, 'users/register.html',context)
+
+
