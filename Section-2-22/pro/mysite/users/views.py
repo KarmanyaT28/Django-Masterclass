48a413468d1579e2f26c2fc38aa2bc7112357927
karmanya <karmanya100@gmail.com>
2026-01-12 00:23:00 -0500

    Section-11 Completed

--- Section-2-22/pro/mysite/users/views.py
+++ Section-2-22/pro/mysite/users/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-
 # from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib import messages
-
 from .forms import RegisterForm
+from django.contrib.auth import logout
 
-
+from django.contrib.auth.decorators import login_required
 
 def register(request):
 
@@ -15,7 +13,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Welcome {username}, your account has been successfully created')
+            messages.success(request,f'Welcome {username}, you have successfully logged in!')
 
             return redirect('login')
     
@@ -27,3 +25,11 @@ def register(request):
     return render(request, 'users/register.html',context)
 
 
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
+
+@login_required
+def profile_view(request):
+    return render(request, 'users/profile.html')
